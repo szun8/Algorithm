a068515504c3261e92f8baf8d15ac5fb1994e97b
樂 <chse0810@gmail.com>
2021-10-10 23:11:11 +0900

    합

---
+++ P.for,while/합.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int input;
+    cin >> input;
+
+    int sum=0;
+
+    for(int i =1;i<=input;++i){
+        sum+=i;
+    }
+    cout<<sum;
+    
+    return 0;
+}
