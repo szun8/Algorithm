6a9feb12ab30c6cf1c82e99b879c8d4de35f2252
樂 <chse0810@gmail.com>
2021-10-13 16:13:00 +0900

    최소와최대

---
+++ P.array/최소와최대.cpp
@@ -0,0 +1,21 @@
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+// using namespace std;
+
+// int main(){
+//     int input;
+//     cin >> input;
+
+//     int num;
+//     vector<int> nums;
+//     for(int i = 0;i<input;++i){
+//         cin >> num;
+//         nums.push_back(num);
+//     }
+//     int max = *max_element(nums.begin(), nums.end());
+//     int min = *min_element(nums.begin(), nums.end());
+
+//     cout << min << " " << max;
+//     return 0;
+// }
