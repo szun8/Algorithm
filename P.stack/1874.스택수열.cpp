1f3910e4ea1fd388e58adb3eaa48efad06e28094
樂 <chse0810@gmail.com>
2021-10-24 03:29:37 +0900

    1874.스택수열

---
+++ P.stack/1874.스택수열.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    scanf("%d",&n);
+    vector<int> line;
+
+    int rule[100001];
+    vector<char> out;
+
+    int count=0;    // stack에 들어가는 수 count
+    int ruleCount=0;    // 임의 수열이 체크됐는지 아닌지 확인
+    for(int i=0; i<n;++i){
+        int num;
+        scanf("%d",&num);
+        rule[i] = num;
+    }
+
+    for (int i = 0; i <= n; i++)
+    {
+        line.push_back(++count);
+        out.push_back('+');
+        while (!line.empty() && line.back() == rule[ruleCount]) {
+            line.pop_back();
+            out.push_back('-'); 
+            ruleCount++;
+        }
+    }
+    
+
+    if(!line.empty()) printf("NO");
+    else{
+        for(int i = 0;i<out.size();++i){
+            cout<<out[i]<<"\n";
+        }
+    }
+    return 0;
+}
