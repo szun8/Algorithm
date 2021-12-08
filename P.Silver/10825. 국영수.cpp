239d1eae3c039e0bed70b598555b0b1a832b027f
최시은 <chse0810@gmail.com>
2021-12-08 19:09:14 +0900

    10825. class name 추가

--- P.Silver/10825. 국영수.cpp
+++ P.Silver/10825. 국영수.cpp
@@ -5,44 +5,46 @@ using namespace std;
 
 class score {
 public:
+    string name;
     int korean;
     int english;
     int math;
 
 public:
-    score(int k, int e, int m){
+    score(string n, int k, int e, int m){
+        name=n;
         korean=k;
         english=e;
         math=m;
     }
 };
 
-bool cmp(pair<string,score> a, pair<string,score> b){
-    if(a.second.korean != b.second.korean) return a.second.korean > b.second.korean;    //국어점수가 감소하는 순 = 내림차순
-    else if(a.second.english != b.second.english) return a.second.english < b.second.english; //영어점수가 증가하는 순 = 오름차순
-    else if(a.second.math != b.second.math) return a.second.math > b.second.math;   //수학점수가 감소하는 순 = 내림차순
-    else return a.first < b.first;  //사전순
+bool cmp(score a, score b){
+    if(a.korean != b.korean) return a.korean > b.korean;    //국어점수가 감소하는 순 = 내림차순
+    else if(a.english != b.english) return a.english < b.english; //영어점수가 증가하는 순 = 오름차순
+    else if(a.math != b.math) return a.math > b.math;   //수학점수가 감소하는 순 = 내림차순
+    else return a.name < b.name;  //사전순
 }   
 
 int main(){
     int n;
     cin>>n;
-    vector<pair<string,score> > student;
+    vector<score> student;
     for (int i = 0; i < n; i++)
     {
         string name;
         cin>>name;
         int K, E, M;
         cin>>K>>E>>M;
-        score person(K,E,M);
+        score person(name,K,E,M);
 
-        student.push_back(pair<string,score>(name, person));
+        student.push_back(person);
     }
     stable_sort(student.begin(),student.end(),cmp);
 
     for (int i = 0; i < n; i++)
     {
-        cout<<student[i].first<<"\n";
+        cout<<student[i].name<<"\n";
     }
     
     return 0;
