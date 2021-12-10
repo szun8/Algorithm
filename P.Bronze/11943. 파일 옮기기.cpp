a52ee181708e80aa3ee94cf3bc77e5232b9fceba
최시은 <chse0810@gmail.com>
2021-12-10 23:21:03 +0900

    11943. 최종수정

--- P.Bronze/11943. 파일 옮기기.cpp
+++ P.Bronze/11943. 파일 옮기기.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main(){
     int a,b,c,d;
     cin>>a>>b;
     cin>>c>>d;
-    int answer = 0;
-    if(a>c) answer = c;
-    else answer = a;
-
-    if(b>d) answer+=d;
-    else answer+=b;
-
-    cout<<answer; 
+    int answer1 = a+d;
+    int answer2 = b+c;
+    
+    cout<<min(answer1,answer2);
     return 0;
 }
