6c9df8abeaa478fb1557886a9356852e7b22cd4a
최시은 <chse0810@gmail.com>
2021-12-13 20:17:33 +0900

    2309. 일곱난쟁이

---
+++ P.Bronze/2309. 일곱난쟁이.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+using namespace std;
+int main(){
+    vector<int> heights(9);
+    for (int i = 0; i < 9; i++) {
+        cin>>heights[i];
+    }
+    sort(heights.begin(),heights.end());
+    int sum = accumulate(heights.begin(),heights.end(),0);
+    int sub,tmp1,tmp2;
+    for (int i = 0; i < 9; i++) {
+        for (int j = i+1; j < 9; j++) {
+            sub = heights[i]+heights[j];
+            if(sum - sub == 100) {
+                tmp1 = i;
+                tmp2 = j;
+                i=9;
+                break;
+            }
+        }
+    }
+
+    for (int z = 0; z < 9; z++) {
+        if(z == tmp1 || z == tmp2) continue;
+        else {
+            cout<<heights[z]<<"\n";
+        }
+    }
+    return 0;
+}
