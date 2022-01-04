bbfe7999ac157cb661dd506bd969e2db4d880030
최시은 <chse0810@gmail.com>
2022-01-04 15:55:08 +0900

    1927. 최소 힙
    우선순위 큐

---
+++ P.Silver/1927. 최소 힙.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n; cin>>n;
+    priority_queue<ll,vector<ll>, greater<ll> > pq;
+    while (n--)
+    {
+        ll what; cin>>what;
+        if(what==0){
+            if(pq.empty()) cout<<0<<"\n";
+            else {
+                cout<<pq.top()<<"\n";
+                pq.pop();
+            }
+        }
+        else {
+            pq.push(what);
+        }
+    }
+    return 0;
+}
