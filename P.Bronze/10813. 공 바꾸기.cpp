5e7bc73bc4f90345515a74092ed4fad06e081d33
최시은 <chse0810@gmail.com>
2022-01-10 22:37:20 +0900

    10813. 공 바꾸기

---
+++ P.Bronze/10813. 공 바꾸기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int N,M; cin>>N>>M;
+    vector<int> basket(N+1);
+    for (int i = 1; i < N+1; i++) basket[i]=i;
+    
+    while(M--){
+        int i,j,temp; cin>>i>>j;
+        temp=basket[i];
+        basket[i]=basket[j];
+        basket[j]=temp;
+    }
+    for (int i = 1; i < N+1; i++) cout<<basket[i]<<" ";
+    
+    return 0;
+}
