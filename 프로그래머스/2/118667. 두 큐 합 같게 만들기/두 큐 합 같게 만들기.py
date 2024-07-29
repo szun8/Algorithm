b5f613698569d84f2c4438dad5d8e7acff7e70c5
Sieun Choi <76219149+szun8@users.noreply.github.com>
2024-07-29 22:51:54 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 76.24 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+def solution(queue1, queue2):
+    que1Sum = sum(queue1)
+    que2Sum = sum(queue2)
+
+    needNum = int((que1Sum + que2Sum) / 2 )
+
+    # 더 큰 sum 가려내기
+    if(que1Sum > que2Sum):
+        moreSum = que1Sum
+        moreQue = deque(queue1)
+        lessQue = deque(queue2)
+    else:
+        moreSum = que2Sum
+        moreQue = deque(queue2)
+        lessQue = deque(queue1)
+
+    diffSum = moreSum - needNum
+    maxCnt = len(queue1) * 3
+
+    cnt = 0
+    while(diffSum != 0):
+        if(cnt > maxCnt):
+            cnt = -1
+            break
+
+        if(diffSum > 0):
+            tmp = moreQue.popleft()
+            cnt += 1
+            lessQue.append(tmp)
+            diffSum -= tmp
+
+        elif(diffSum < 0):
+            tmp = lessQue.popleft()
+            cnt += 1
+            moreQue.append(tmp)
+            diffSum += tmp
+        
+    return cnt
