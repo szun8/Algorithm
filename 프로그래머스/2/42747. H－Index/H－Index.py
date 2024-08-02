6819ccb42f74334420d91b3fc0bc5adc9a22fe38
Sieun Choi <76219149+szun8@users.noreply.github.com>
2024-08-02 17:37:09 +0900

    [level 2] Title: H-Index, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,18 +1,8 @@
+
 def solution(citations):
-    answer = 0
-    citations.sort() #오름차순 #nlogn
-    length = len(citations)
-
-    for h in range(length+1):   #n^2
-        i = 0
-        while i < length:
-            if(citations[i] >= h and length-i >= h):
-                if(i>0 and i-1 <= h):
-                    answer = h
-                    break
-                elif i is 0:
-                    answer = h
-                    break
-            i += 1
-
-    return answer
+    citations = sorted(citations)
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l-i:
+            return l-i
+    return 0
