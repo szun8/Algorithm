ff5c96bb52b49a0df2563179f8a5e93ea6882ce5
Sieun Choi <76219149+szun8@users.noreply.github.com>
2024-08-02 17:25:13 +0900

    [level 2] Title: H-Index, Time: 11.76 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,16 +1,18 @@
 def solution(citations):
     answer = 0
-    citations.sort() #오름차순
-    maxCnt = min(len(citations), citations[-1])
+    citations.sort() #오름차순 #nlogn
     length = len(citations)
-    for h in range(maxCnt+1):
-        for i in range(length):
+
+    for h in range(length+1):   #n^2
+        i = 0
+        while i < length:
             if(citations[i] >= h and length-i >= h):
-                if(i>0 and i <= h):
+                if(i>0 and i-1 <= h):
                     answer = h
                     break
                 elif i is 0:
                     answer = h
                     break
+            i += 1
 
     return answer
