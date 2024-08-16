2ae9146b65e2b02b7284994d31205b9da6482dde
Sieun Choi <76219149+szun8@users.noreply.github.com>
2024-08-16 23:44:29 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 1228 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,30 @@
+#include <vector>
+#include <string>
+using namespace std;
+int n, m;
+void DFS(vector<int> curN, string answer, int cnt){
+    if(cnt == m){
+        for(int i=0; i<answer.size();i++){
+            printf("%c ", answer[i]);
+        }
+        printf("\n");
+    }
+    else{
+        for(int j=0; j<curN.size();j++){
+            if(curN[j]==-1) continue;
+            int addNum = curN[j];
+            curN[j] = -1;
+            DFS(curN, answer+to_string(addNum), cnt+1);
+        }
+    }
+}
+
+int main(){
+    scanf("%d %d", &n, &m);
+    vector<int> N;
+    for(int i=1; i<=n;i++){
+        N.push_back(i);
+    }
+    DFS(N, "", 0);
+    return 0;
+}
