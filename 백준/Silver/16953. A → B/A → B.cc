1aea4367de8bc29e53b63391c80e5010ebd7c84b
Sieun Choi <76219149+szun8@users.noreply.github.com>
2024-08-18 22:22:14 +0900

    Update A → B.cc
    예전 버전의 코드가 올라가는 이슈 해결

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -1,47 +1,45 @@
-#include <iostream>
 #include <queue>
-using namespace std;
-long long a,b,answer;
-bool isOk = false;
-class CountNumber{
-public:
-    long long num, cnt;
-    CountNumber(long long n, long long c){
-        num=n; cnt=c;
-    }
-};
-queue<CountNumber> number;
+#include <iostream>
 
-void BFS(){
+#define MAX 1000000000
+
+struct Node{
+public:
+    long long num;
+    int cnt;
+public:
+    Node(long long a, int b) : num(a), cnt(b){};
+};
+// 1000000000
+// 430000000 503270511
+int BFS(int src, int target){
+    int answer = -1;
+    std::queue<Node> number;
+    number.push(Node(src, 0));
+
     while(!number.empty()){
-        long long num = number.front().num;
-        long long cnt = number.front().cnt;
+        Node curNum = number.front();
         number.pop();
-        CountNumber cntnum1(num*2,cnt+1);
-        if(cntnum1.num==b){
-            isOk=true;
-            answer=cnt;
-            return ;
+        if(curNum.num > MAX) break;
+        else if(curNum.num > target) continue;
+        else if(curNum.num == target) {
+            answer = curNum.cnt;
+            break;
         }
-        else if(cntnum1.num < b) number.push(cntnum1);
-        CountNumber cntnum2((num*10)+1,cnt+1);
-        if(cntnum2.num==b){
-            isOk=true;
-            answer=cnt;
-            return ;
-        }
-        else if(cntnum2.num < b) number.push(cntnum2);
+
+        if(curNum.num*2 > MAX) continue;
+        number.push(Node(curNum.num*2, curNum.cnt+1));      // *2 
+
+        if(curNum.num*10+1 > MAX) continue;     // 피연산자의 자료형을 기준으로 연산을 진행해야한다. 그래야 오버플로우가 발생하지 않음
+        number.push(Node(curNum.num*10+1, curNum.cnt+1));   // *10 + 1
     }
+    if(answer > -1) return answer+1;
+    else return -1;
 }
+
 int main(){
-    cin>>a>>b;
-    if(a==b) {
-        cout<<1;
-        return 0;
-    }
-    CountNumber cntnum(a,1);
-    number.push(cntnum);
-    BFS();
-    if(isOk==true) cout<<answer+1;
-    else cout<<-1;
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d",BFS(a, b));
+    return 0;
 }
