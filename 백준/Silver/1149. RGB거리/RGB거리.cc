dab5b81903db079b1c17300636a5520f2149fa91
Sieun Choi <76219149+szun8@users.noreply.github.com>
2024-09-18 20:56:02 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    vector<vector<int> > rgb(n);
+    vector<vector<int> > rgbCost(n, vector <int>(3,0));
+    for(int i=0; i<n; i++){
+        for(int j=0; j<3; j++){
+            int cost;
+            cin >> cost;
+            rgb[i].push_back(cost);
+        }
+    }
+
+    rgbCost[0][0] = rgb[0][0];
+    rgbCost[0][1] = rgb[0][1];
+    rgbCost[0][2] = rgb[0][2];
+
+    for(int i=1; i<n; i++){
+        for(int j=0; j<3; j++){
+            if(j==0){
+                rgbCost[i][j] = min(rgbCost[i-1][j+1], rgbCost[i-1][j+2])+ rgb[i][j];
+            }
+            else if(j==1){
+                rgbCost[i][j] = min(rgbCost[i-1][j-1], rgbCost[i-1][j+1])+ rgb[i][j];
+            }
+            else if(j==2){
+                rgbCost[i][j] = min(rgbCost[i-1][j-1], rgbCost[i-1][j-2])+ rgb[i][j];
+            }
+        }
+    }
+
+    cout<<*min_element(rgbCost[n-1].begin(), rgbCost[n-1].end());
+    return 0;
+}
