587d4db8a6f671b13cbce3109a072272eb3aa110
Sieun Choi <76219149+szun8@users.noreply.github.com>
2024-10-17 16:55:55 +0900

    [Gold IV] Title: 최단경로, Time: 84 ms, Memory: 9192 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -15,9 +15,10 @@ void Dijkstra(vector<vector<pair<int, int> > > & graph, int start, vector<int>& 
     dist[start] = 0;
     pq.push({0, start});
 
-    int curNode, nextNode, nextWeight;
+    int curNode, curWeight, nextNode, nextWeight;
     while(!pq.empty()){
         curNode = pq.top().second;
+        curWeight = pq.top().first;
         pq.pop();
         
         for(pair<int,int> node : graph[curNode]){
@@ -45,14 +46,14 @@ int main(){
     int from, to, weight;
     for(int i=0; i<e;i++){
         cin>>from>>to>>weight;
-        auto iter = find_if(graph[from].begin(), graph[from].end(), [to](pair<int,int> i){ return i.first == to; });
-        if(iter != graph[from].end()){
-            if(iter->second > weight){
-                iter->second = weight;
-            } 
-        }else{
+        // auto iter = find_if(graph[from].begin(), graph[from].end(), [to](pair<int,int> i){ return i.first == to; });
+        // if(iter != graph[from].end()){
+        //     if(iter->second > weight){
+        //         iter->second = weight;
+        //     } 
+        // }else{
             graph[from].push_back({to, weight});
-        }
+        //}
     }
 
     Dijkstra(graph, k, dist);
