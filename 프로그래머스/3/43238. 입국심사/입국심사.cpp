5f3ad15574c7cda7fe46137181fd5345de7c2213
Sieun Choi <76219149+szun8@users.noreply.github.com>
2024-10-26 01:45:40 +0900

    [level 3] Title: 입국심사, Time: 40.15 ms, Memory: 6.72 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    sort(times.begin(), times.end());
+
+    long long mid, left = times[0], right = (long long)times.back() * n;
+
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+
+        long long possibleClientCnt = 0;
+        for (int i = 0; i < times.size(); i++) {
+            possibleClientCnt += (mid / times[i]);
+            if (possibleClientCnt >= n) break;
+        }
+        if (possibleClientCnt < n) {  // 현재 기준 시간으로는 모든 고객을 받지 못하므로 기준 시간을 높여야함
+            left = mid + 1; // left 갱신
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
