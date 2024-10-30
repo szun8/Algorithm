cb5277bfd251015d8d58973b5ccc29869b25b778
Sieun Choi <76219149+szun8@users.noreply.github.com>
2024-10-31 01:45:15 +0900

    [level 3] Title: 이중우선순위큐, Time: 1155.21 ms, Memory: 47.7 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cs
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cs
@@ -5,22 +5,20 @@ using System.Linq;
 public class Solution {
     public int[] solution(string[] operations) {
         int[] answer = new int[] {0,0};
-        LinkedList<int> arr = new LinkedList<int>();
+        List<int> arr = new List<int>();
         
         foreach(string operation in operations){
             if(operation[0] == 'I'){
                 int num = int.Parse(operation.Substring(2));
-                arr.AddLast(num);
-                List<int> tmp = new List<int>(arr);
-                tmp.Sort(); // 오름차순
-                arr = new LinkedList<int>(tmp);
+                arr.Add(num);
+                arr.Sort();
             }
             else{
                 if(arr.Count == 0) continue;
                 else if(int.Parse(operation.Substring(2)) == 1){
-                    arr.RemoveLast();
+                    arr.Remove(arr.Max());
                 }else{
-                    arr.RemoveFirst();
+                    arr.Remove(arr.Min());
                 }
             }
         }
