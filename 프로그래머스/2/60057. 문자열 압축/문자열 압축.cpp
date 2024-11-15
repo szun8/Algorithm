dd3e7411d7dbef6ac75475c03f3cd1f03b359344
Sieun Choi <76219149+szun8@users.noreply.github.com>
2024-11-16 02:11:15 +0900

    [level 2] Title: 문자열 압축, Time: 0.91 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
@@ -1,51 +1,38 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <unordered_map>
-
 using namespace std;
 
 int solution(string s) {
-    unordered_map<string, int> compression;
     int strSize = s.length();
     int answer = strSize;
-
+    int cnt = 0;
     for (int i = 1; i <= strSize; i++) {
-        compression.clear();
+        cnt = 0;
         string result = "";
         string beforeStr = "";
         string restStr = "";
         for (int j = 0; j < strSize; j += i) {
             if (strSize - j >= i) { // i크기 만큼 자를 수 있는 경우
                 string tmp = s.substr(j, i);
-                if (compression.find(tmp) == compression.end()) {
-                    compression[tmp] = 1;
+                if (beforeStr == "") {
+                    cnt = 1; // reset
                 }
-                else {
-                    if (tmp != beforeStr) {
-                        // 이미 있는데 이전 문자열과 다르다면, 중복이지만 연속적인 문자가 아님 -> 여태까지 결과 축적해서 클리어 시키기
-                        for (auto it = compression.begin(); it != compression.end(); it++) {
-                            if (it->second == 1) result += it->first;
-                            else result += (to_string(it->second) + it->first);
-                        } compression.clear();
-                        compression[tmp] = 1;
-                    }
-                    else compression[tmp]++;
+                else if (beforeStr != tmp) {
+                    if (cnt == 1) result += beforeStr;
+                    else result += (to_string(cnt) + beforeStr);
+                    cnt = 1; // reset
                 }
+                else cnt++;
                 beforeStr = tmp;
             }
             else restStr = s.substr(j);
         }
-        for (auto it = compression.begin(); it != compression.end(); it++) {
-            if (it->second == 1) result += it->first;
-            else result += (to_string(it->second) + it->first);
-        } result += restStr;
-        if (answer > result.length()) answer = result.length();
+
+        if (cnt == 1) result += beforeStr;
+        else result += (to_string(cnt) + beforeStr);
+
+        result += restStr; // 나머지 문자열이 있다면 합쳐주기
+        if (answer > result.length()) answer = result.length(); // 결과 갱신
     }
     return answer;
-}
-
-int main() {
-    solution("abcde");
-    return 0;
 }
